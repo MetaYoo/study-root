ca8d4eab4a29c49283299cf8bb78500a2b62a8d6
aracwong <aracwong@163.com>
2020-07-12 10:33:28 +0800

    init commit

---
+++ study-springsecurity/src/main/java/com/github/aracwong/AppServer.java
@@ -0,0 +1,8 @@
+package com.github.aracwong;
+
+@SpringBootApplication
+public class AppServer {
+    public static void main(String[] args) {
+         SpringApplication.run(AppServer.class, args);
+    }
+}
